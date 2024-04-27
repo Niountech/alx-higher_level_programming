862906eccf1661ef057cfbca3c99d0ff78b77c3b
Nduke  Sunday <155595702+Niountech@users.noreply.github.com>
2024-04-27 13:12:51 +0100

    Update 1-hbtn_header.py

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -6,18 +6,8 @@ if __name__ == '__main__':
     import sys
     import urllib.request
 
-    if len(sys.argv) != 2:
-        print("Usage: python script.py <URL>")
-        sys.exit(1)
-
     url = sys.argv[1]
 
-    try:
-        with urllib.request.urlopen(url) as r:
-            request_id = r.getheader('X-Request-Id')
-            if request_id:
-                print(request_id)
-            else:
-                print("X-Request-Id header not found in the response.")
-    except Exception as e:
-        print("An error occurred:", e)
+    with urllib.request.urlopen(url) as r:
+        print(r.getheader('X-Request-Id'))
+
